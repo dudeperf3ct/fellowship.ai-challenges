a432ec5e7c507b082e34943af1f1792e2729c7e6
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2019-03-30 02:49:49 +0530

    Add files via upload

---
+++ transfer_learning_sentiment/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We will use transfer learning approaches in NLP like CoVe, ELMo, BERT and GPT on Twitter US Airline Dataset.',
+    author='dudeperf3ct',
+    license='MIT',
+)
